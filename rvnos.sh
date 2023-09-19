a03f07e7b326c81d52afce81d866a0c46f5fbc48
mblanka23 <144958444+mblanka23@users.noreply.github.com>
2023-09-19 14:41:43 +1000

    rvnos.sh

--- rvnos.sh
+++ rvnos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-read -p "YOUR WALLET ADDRESS: " wallet_address && sudo  apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y && git clone https://github.com/tpruvot/cpuminer-multi && cd cpuminer-multi && ./build.sh && ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u $wallet_address.trx -p c=RVN --coin monero -a rx/1,n=I_TA_VPS,o=VPS -k -t 8
+read -p "YOUR WALLET ADDRESS: " wallet_address && sudo  apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y && git clone https://github.com/tpruvot/cpuminer-multi && cd cpuminer-multi && ./build.sh && ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u $wallet_address.trx -p c=RVN -a rx/1,n=I_TA_VPS,o=VPS -k -t 8
