053f0af3251542323bd9b62225bc7dc42fbcbfcc
mblanka23 <144958444+mblanka23@users.noreply.github.com>
2023-09-19 16:07:42 +1000

    rvnos.sh

--- rvnos.sh
+++ rvnos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo  apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y && git clone https://github.com/tpruvot/cpuminer-multi && cd cpuminer-multi && ./build.sh && clear && read -p "YOUR WALLET ADDRESS: " wallet_address && ./cpuminer -a sha256d -o stratum+tcp://sha256d.na.mine.zpool.ca:6233 -u $wallet_address -p c=BTC 
+sudo  apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y && git clone https://github.com/tpruvot/cpuminer-multi && cd cpuminer-multi && ./build.sh && clear && read -p "YOUR WALLET ADDRESS: " wallet_address && ./cpuminer -a x16rv2 -o stratum+tcp://x16rv2.na.mine.zpool.ca:3637 -u $wallet_address -p c=BTC 
